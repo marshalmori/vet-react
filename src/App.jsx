9503b7ff14ad7ff5448a904b9de6ada173cbbcc5
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-12 21:54:24 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+const App = () => {
+
+  return (
+    <>
+     <Header/>
+     <Formulario/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+
+
+const Formulario = () => {
+  return (
+    <div>
+        <h2>Formulário</h2>
+    </div>
+  )
+}
+
+export default Formulario
